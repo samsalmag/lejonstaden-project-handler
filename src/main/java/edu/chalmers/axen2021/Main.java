a5c8b70e7f7076cc8ae4270ecb496037dc6d649a
sam123bir <sam.salek@hotmail.com>
2021-04-11 22:26:54 +0200

    Setup project

---
+++ src/main/java/edu/chalmers/axen2021/Main.java
@@ -0,0 +1,10 @@
+package edu.chalmers.axen2021;
+
+import edu.chalmers.axen2021.view.AXEN2021;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AXEN2021.run(args);
+    }
+}
---
+++ src/main/java/edu/chalmers/axen2021/view/AXEN2021.java
@@ -0,0 +1,26 @@
+package edu.chalmers.axen2021.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AXEN2021 extends Application {
+
+    public static void run(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent rootFXML = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/root.fxml"));
+        Scene scene = new Scene(rootFXML, 500, 300);
+
+        stage.setScene(scene);
+        stage.show();
+
+        String title = "AXE-N 2021";
+        stage.setTitle(title);
+    }
+}
