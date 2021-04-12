60c3c04a1ed438415cd7ba52a42ffc5d95fc0462
Tilda Grönlund <tildagronlund@Tildas-MacBook-Pro.local>
2021-04-12 10:06:34 +0200

    hej

--- src/main/java/edu/chalmers/axen2021/Main.java
+++ src/main/java/edu/chalmers/axen2021/Main.java
@@ -3,6 +3,7 @@ package edu.chalmers.axen2021;
 import edu.chalmers.axen2021.view.AXEN2021;
 
 public class Main {
+	String title = "hej";
 
     public static void main(String[] args) {
         AXEN2021.run(args);
