bb81a6bbecaee192111ce8c890710ef3cb863d1a
tildagronlund <tildafm.gronlund@gmail.com>
2021-04-20 11:48:35 +0200

    Made the ApartmentData class

---
+++ src/main/java/edu/chalmers/axen2021/model/ApartmentData.java
@@ -0,0 +1,58 @@
+package edu.chalmers.axen2021.model;
+
+/**
+ * Class for calculating the apartment data.
+ * @author Ahmad Al-Aref
+ * @author Sara Vardheim
+ * @author Tilda Grönlund
+ */
+public class ApartmentData {
+    private SaboTable sabo;
+
+    public ApartmentData() {
+        this.sabo = new SaboTable();
+    }
+
+    /**
+     * Calculates and returns monthly rent.
+     * @param type apartment type, no of rooms
+     * @param rent normrent if low, presumed rent if high
+     * @param boa living area of the apartment
+     * @return high or low monthly rent depending on the rent
+     */
+    public double getMonthlyRent(String type, int rent, double boa) {
+        double rh = (rent*boa)/(sabo.getRE(type)+boa);
+        return (sabo.getRE(type)+boa)*rh/12;
+    }
+
+    /**
+     * Calculates and returns kr per sqm of the apartment.
+     * @param type apartment type, no of rooms
+     * @param rent normrent if low, presumed rent if high
+     * @param boa living area of the apartment
+     * @return kr per sqm of the apartment
+     */
+    public double getKrPerSqm(String type, int rent, double boa) {
+        return getMonthlyRent(type, rent, boa)*12/boa;
+    }
+
+    /**
+     * Calculates and returns subsidy for specified apartment type.
+     * @param investmentSub investment subsidy
+     * @param quantity  no of apartments of specified type
+     * @param boa living area of the apartment
+     * @return subsidy for specified apartment type
+     */
+    public double getSubsidy(double investmentSub, int quantity, double boa) {
+        double x;
+        if(boa > 70)
+            x = 35*investmentSub+(35*investmentSub)/2;
+        else if (boa > 35)
+            x = 35*investmentSub+((boa-35)*investmentSub)/2;
+        else
+            x = boa*investmentSub;
+
+        return x/1000*quantity*1.75;
+    }
+
+}
