56fb23ec87e251052f5787ee3004ad29991977f1
Erik Wetter <erikwetter13@gmail.com>
2021-04-27 19:22:12 +0200

    Added functionality to buttons in addNewProjectView.fxml.

--- src/main/java/edu/chalmers/axen2021/controller/addNewProjectController.java
+++ src/main/java/edu/chalmers/axen2021/controller/addNewProjectController.java
@@ -1,4 +1,33 @@
 package edu.chalmers.axen2021.controller;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class addNewProjectController {
+
+    private RootController rootController = RootController.getInstance();
+
+    @FXML private TextField projektnamnTextField;
+
+    /**
+     * Method for closing the modalWindow.
+     * Puts the modalWindowAnchorPane to back in scene.
+     * @param event that triggered the method.
+     */
+    @FXML
+    private void closeAddNewProjectView(Event event) {
+        rootController.getAddNewProjectAnchorPane().toBack();
+        projektnamnTextField.clear();
+    }
+
+    @FXML
+    private void addNewProject() throws IOException {
+        rootController.getSideBarController().addNewProject();
+        rootController.getAddNewProjectAnchorPane().toBack();
+        projektnamnTextField.clear();
+    }
+
 }
