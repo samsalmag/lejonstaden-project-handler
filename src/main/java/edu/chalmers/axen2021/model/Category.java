5820e6c2f7c2e5fde50aef37c5f52907e8510dd1
sam123bir <sam.salek@hotmail.com>
2021-04-28 13:11:43 +0200

    Added JavaDocs to Category class.

--- src/main/java/edu/chalmers/axen2021/model/Category.java
+++ src/main/java/edu/chalmers/axen2021/model/Category.java
@@ -1,5 +1,9 @@
 package edu.chalmers.axen2021.model;
 
+/**
+ * Enum class for the different (modal) categories.
+ * @author Sam Salek
+ */
 public enum Category {
     TOMTKOSTNADER("Tomtkostnader"),
     NEDLAGDABYGGHERRE("Nedlagda bygherre"),
@@ -16,16 +20,20 @@ public enum Category {
     DRIFTNETTO("Driftnetto"),
     YIELD("Yield");
 
+    /**
+     * String name of the category. Must be the same as the string on the category buttons in the input view.
+     */
     private String name;
 
     Category(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
+    /**
+     * Gets a Category from a string input.
+     * @param text the category to get.
+     * @return Category found from the string.
+     */
     public static Category fromString(String text) {
         for (Category category : Category.values()) {
             if (category.name.equalsIgnoreCase(text)) {
@@ -33,5 +41,13 @@ public enum Category {
             }
         }
         throw new IllegalArgumentException("No enum Category value with name " + text + " found");
+    }
+
+    /**
+     * Gets the name of the Category.
+     * @return name variable.
+     */
+    public String getName() {
+        return name;
     }
 }
