5fd399a5da0616e48db2d367a10b9ff55ff6eb50
oscarvid <o.j.arvidson@hotmail.com>
2021-05-04 19:16:38 +0200

    Fixed TextField property for ApartmentTypeController.java

--- src/main/java/edu/chalmers/axen2021/controller/ApartmentTypeController.java
+++ src/main/java/edu/chalmers/axen2021/controller/ApartmentTypeController.java
@@ -1,12 +1,15 @@
 package edu.chalmers.axen2021.controller;
 
 import edu.chalmers.axen2021.model.ApartmentType;
+import edu.chalmers.axen2021.utils.StringUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 @FXMLController
@@ -20,6 +23,8 @@ public class ApartmentTypeController implements Initializable {
 
     private ApartmentType apartmentType;
 
+    private ArrayList<TextField> inputFields = new ArrayList<TextField>();
+
     public ApartmentTypeController(ApartmentType apartmentType) {
         this.apartmentType = apartmentType;
     }
@@ -29,30 +34,58 @@ public class ApartmentTypeController implements Initializable {
         BOATextField.setText(String.valueOf(apartmentType.getBOA()));
         amountTextField.setText(String.valueOf(apartmentType.getAmount()));
 
-        initListeners();
+        initTextFieldProperties();
     }
 
-    private void initListeners() {
-        BOATextField.textProperty().addListener(((observableValue, oldValue, newValue) -> {
 
-            if(!newValue.matches("\\d*")){
-                BOATextField.setText(newValue.replaceAll("[^\\d]", ""));
+    /**
+     * Init method for input TextFields properties.
+     * Adds focus lost property.
+     * Adds only allowing doubles property.
+     */
+    private void initTextFieldProperties(){
+
+        //Adds property to TextField allowing users to only input numbers and ".".
+        BOATextField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            if(!newValue.matches("[0-9]*" + "[.]?" + "[0-9]*")){
+                BOATextField.setText(oldValue);
             }
+        });
 
-            if(!BOATextField.getText().isEmpty()) {
+        //Adds focus lost property to textFields.
+        BOATextField.focusedProperty().addListener((observableValue, oldValue, newValue) -> {
+            if(!newValue){
+                //Make sure that the textField has a readable value.
+                if(BOATextField.getText().equals("") || BOATextField.getText().equals(".")){
+                    BOATextField.setText("0.0");
+                }
+                // ToDo remove comment when method implemented.
+                // BOATextField.setText(StringUtils.removeTrailingZeros(BOATextField.getText()));
                 apartmentType.setBOA(Double.parseDouble(BOATextField.getText()));
+
             }
-        }));
+        });
 
-        amountTextField.textProperty().addListener(((observableValue, oldValue, newValue) -> {
-
+        //Adds property to TextField allowing users to only input numbers and.
+        amountTextField.textProperty().addListener((observableValue, oldValue, newValue) -> {
             if(!newValue.matches("\\d*")){
-                amountTextField.setText(newValue.replaceAll("[^\\d]", ""));
+                amountTextField.setText(oldValue);
             }
+        });
 
-            if(!amountTextField.getText().isEmpty()) {
+        //Adds focus lost property to textFields.
+        amountTextField.focusedProperty().addListener((observableValue, oldValue, newValue) -> {
+            if(!newValue){
+                //Make sure that the textField has a readable value.
+                if(amountTextField.getText().equals("")){
+                    amountTextField.setText("0");
+                }
+                // ToDo remove comment when method implemented.
+                // amountTextField.setText(StringUtils.removeTrailingZeros(amountTextField.getText()));
                 apartmentType.setAmount(Integer.parseInt(amountTextField.getText()));
+
             }
-        }));
+        });
     }
 }
+
