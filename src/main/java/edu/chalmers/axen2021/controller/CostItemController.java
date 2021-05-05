2bd089984a540009b8891172d40a81a22203b176
oscarvid <o.j.arvidson@hotmail.com>
2021-05-05 15:54:51 +0200

    Added functionality and button for removing a CostItem.

--- src/main/java/edu/chalmers/axen2021/controller/CostItemController.java
+++ src/main/java/edu/chalmers/axen2021/controller/CostItemController.java
@@ -40,10 +40,7 @@ public class CostItemController implements Initializable {
      */
     private CostItem costItem;
 
-    /**
-     * Boolean for including moms.
-     */
-    private Boolean includeMoms;
+    private RootController rootController = RootController.getInstance();
 
     public CostItemController(CostItem costItem) {
         this.costItem = costItem;
@@ -102,5 +99,13 @@ public class CostItemController implements Initializable {
      */
     public Boolean momsIsSelected() {
         return momsCheckBox.isSelected();
+    }
+
+    /**
+     * Removes cost item.
+     */
+    @FXML
+    private void removeCostItem(){
+        rootController.removeCostItem(costItem.getName());
     }
 }
