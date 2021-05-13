42c9a0a736b28ad229749dcf25056dec9f72eb31
sam123bir <sam.salek@hotmail.com>
2021-05-13 11:26:56 +0200

    Added project regex and JavaDocs to StringUtils.

--- src/main/java/edu/chalmers/axen2021/utils/StringUtils.java
+++ src/main/java/edu/chalmers/axen2021/utils/StringUtils.java
@@ -1,19 +1,49 @@
 package edu.chalmers.axen2021.utils;
 
+/**
+ * Utilities for String objects.
+ * @author Malte Åkvist
+ * @author Oscar Arvidson
+ */
 public class StringUtils {
 
+    /**
+     * The regex used on project name input.
+     */
+    public static String projectNameRegex = "^$|^[0-9a-zA-ZåÅäÄöÖ\\s\\^\\&\\'\\@\\{\\}\\[\\]\\,\\$\\=\\!\\-\\#\\(\\)\\%\\+\\~\\_ ]+$";
+
+    /**
+     * Removes trailing/unnecessary zeros from a double.
+     * @param d The double.
+     * @return The new double as a String.
+     */
     public static String removeTrailingZeros(Double d) {
         return String.valueOf(d).replaceAll("[0]*$", "").replaceAll("\\.$", "");
     }
 
+    /**
+     * Removes unnecessary zeros from the beginning of a double.
+     * @param i The double.
+     * @return The new double as a String.
+     */
     public static String removeBeginningZeros(int i) {
         return String.valueOf(i).replaceFirst("^0+(?!$)", "");
     }
 
+    /**
+     * Removes trailing/unnecessary zeros from a String.
+     * @param s The string.
+     * @return The new String.
+     */
     public static String removeTrailingZeros(String s) {
         return s.replaceAll("[0]*$", "").replaceAll("\\.$", "");
     }
 
+    /**
+     * Reformats a String to an acceptable double value.
+     * @param s The string.
+     * @return The double value.
+     */
     public static Double convertToDouble(String s) {
         s = s.replaceAll(",", ".");
         s = s.replaceAll("%", "");
