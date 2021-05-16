51213ebd27596bb14b63c75617f1a3a594ea071f
sam123bir <sam.salek@hotmail.com>
2021-05-16 03:26:48 +0200

    Added a factory class for ApartmentItemController, is used to create instances instead. This class exists so we can update all TextFields through all instances of ApartmentItemController.

---
+++ src/main/java/edu/chalmers/axen2021/controller/items/ApartmentItemControllerFactory.java
@@ -0,0 +1,52 @@
+package edu.chalmers.axen2021.controller.items;
+
+import edu.chalmers.axen2021.model.projectdata.ApartmentItem;
+
+import java.util.ArrayList;
+
+/**
+ * Factory class for ApartmentItemController.
+ * Used to create instances of ApartmentItemController.
+ * Also contains a list of all instances of the controller class.
+ *
+ * @author Sam Salek
+ */
+public class ApartmentItemControllerFactory {
+
+    /**
+     * List of all instances of the controller class.
+     */
+    private static ArrayList<ApartmentItemController> instances = new ArrayList<>();
+
+    // Doesn't need to be instantiated or a have reference. Use methods directly instead.
+    private ApartmentItemControllerFactory(){}
+
+    /**
+     * Creates a new instance of ApartmentItemController.
+     * @param apartmentItem The model ApartmentItem class for this specific controller instance.
+     * @return The newly created instance of ApartmentItemController.
+     */
+    public static ApartmentItemController create(ApartmentItem apartmentItem) {
+        ApartmentItemController aic = new ApartmentItemController(apartmentItem);
+        instances.add(aic);
+        System.out.println(ApartmentItemControllerFactory.getInstances().size());
+        return aic;
+    }
+
+    /**
+     * Removes all instances of the active controllers.
+     * Mostly used when switching between views/projects.
+     * Called to to avoid references to controllers whose views got removed at view/project switch.
+     */
+    public static void clearInstances() {
+        instances.clear();
+    }
+
+    /**
+     * Gets the instances of the active controllers.
+     * @return All instances.
+     */
+    public static ArrayList<ApartmentItemController> getInstances() {
+        return instances;
+    }
+}
